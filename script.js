732d863b737a07d18ee636dba6d8a6402cb8dd19
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2024-07-22 22:18:32 +1000

    bug fixes on the script

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // v1.0: Sid Shardanand - 20240722
 //-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 preText = "http://webcache.googleusercontent.com/search?q=cache:";
-inText = prompt("Enter a URL to get the Cached page!\nHelp: sidshardanand.com", "");
-if(inText == ""){
+inText = prompt("Enter a URL to get the Cached page!\nHelp: sidshardanand.com");
+if(inText === null){
 	inText = window.location.href;
 } 
 window.location.replace(preText + inText);
