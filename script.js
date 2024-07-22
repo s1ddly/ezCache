5aa87f675a4ffc79341c47414d870f3db625f50d
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2024-07-22 22:18:21 +1000

    started on the script

---
+++ script.js
@@ -0,0 +1,10 @@
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+// Plugin Script
+// v1.0: Sid Shardanand - 20240722
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+preText = "http://webcache.googleusercontent.com/search?q=cache:";
+inText = prompt("Enter a URL to get the Cached page!\nHelp: sidshardanand.com", "");
+if(inText == ""){
+	inText = window.location.href;
+} 
+window.location.replace(preText + inText);
