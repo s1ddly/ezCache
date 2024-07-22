092f477f42a212fd1f8f62c8fbbf6675f74e78ee
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2024-07-22 22:44:50 +1000

    cody ready

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ preText = "http://webcache.googleusercontent.com/search?q=cache:";
 inText = prompt("Enter a URL to get the Cached page!\nHelp: sidshardanand.com");
 if(inText == ""){
 	window.location.replace(preText + window.location.href);
-} else if(inText!=null) {
+} else if(inText != null) {
 	window.location.replace(preText + inText);
 }
